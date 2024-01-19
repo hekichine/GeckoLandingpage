c833214df2bfe0907b810760777c7460c4ad2df6
hekichien <chienlx1005@gmail.com>
2024-01-19 08:02:50 +0700

    Update btn_effect.js

--- assets/js/btn_effect.js
+++ assets/js/btn_effect.js
@@ -1,1 +1,8 @@
-document.querySelectorAll('.btn_xkxl').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
+
+
+document.on('DOMContentLoaded',function(){
+  if(window.innerWidth < 1024){
+    return;
+  }
+  document.querySelectorAll('.btn_xkxl').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
+})
